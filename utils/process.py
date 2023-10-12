343e86d15b228af68d5857f2388cd85653ce7a66
iranon <jason.ramirez509@protonmail.com>
2023-10-12 07:27:36 -0400

    Wait until we can get the process ID for chrome to continue

--- utils/process.py
+++ utils/process.py
@@ -2,7 +2,7 @@ import psutil
 import subprocess
 import time
 
-def find_or_start_chromium(logger=None):
+def find_or_start_chromium(logger):
     target_args = "--remote-debugging-port=9222 --remote-allow-origins=*"
     
     for proc in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
@@ -10,39 +10,49 @@ def find_or_start_chromium(logger=None):
             cmdline = ' '.join(proc.info['cmdline'])
             if ('chromium' in proc.info['name'].lower() or 'chrome' in proc.info['name'].lower()) \
             and target_args in cmdline:
-                if logger:
-                    logger.log_event("Found running Chromium process that meets criteria, PID: {}, Commandline string: '{}'".format(proc.info['pid'], cmdline))
+                logger.log_event("Found running Chromium process that meets criteria, PID: {}, Commandline string: '{}'".format(proc.info['pid'], cmdline))
                 return proc.info['pid']
             elif ('chromium' in proc.info['name'].lower() or 'chrome' in proc.info['name'].lower()):
-                if logger:
-                    logger.log_event("Found running Chromium process that does not meet criteria with PID: {}, Commandline string: '{}'; killing it".format(proc.info['pid'], cmdline))
+                ogger.log_event("Found running Chromium process that does not meet criteria with PID: {}, Commandline string: '{}'; killing it".format(proc.info['pid'], cmdline))
                 proc.terminate()
         except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
             pass
     
-    if logger:
-        logger.log_event("Starting new Chromium process with correct arguments.")
-    pipe = subprocess.Popen(["chromium", "--remote-debugging-port=9222", "--remote-allow-origins=*"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    cmd_output = pipe.communicate()
+    logger.log_event("Starting new Chromium process with correct arguments.")
+    process = subprocess.Popen(["chromium", "--remote-debugging-port=9222", "--remote-allow-origins=*"], 
+        stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, universal_newlines=True)
 
-    if logger:
-        logger.log_event("Result of new process was {}".format(cmd_output))
-    
-    # Wait for a short while to ensure the process has started
-    time.sleep(5)
-    
-    for proc in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
-        try:
-            cmdline = ' '.join(proc.info['cmdline'])
-            if ('chromium' in proc.info['name'].lower() or 'chrome' in proc.info['name'].lower()) \
-            and target_args in cmdline:
-                if logger:
-                    logger.log_event("PID of new Chromium process is {}".format(proc.info['pid']))
-                return proc.info['pid']
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    
-    if logger:
-        logger.log_event("Unable to find PID of new Chrome process")
+    # Wait for the process to complete and capture the output
+    stdout, stderr = process.communicate()
+    return_code = process.returncode
 
-    return None
+    if return_code != 0:
+        logger.log_event("Recieved error code {} attempting to open Chromium process. stdout: '{}', stderr: '{}'"
+            .format(return_code, stdout, stderr))
+        return None
+    else:
+        logger.log_event("Result from open Chromium process was: '{}'".format(stdout))
+    
+
+    # At this point we know there wasn't an error in starting the process, so it might just be taking
+    # some time to finish start. However, we don't want to keep trying forever. 500 seconds is probably
+    # too long but anything else is gonna break my OCD
+    chromium_pid = None
+    max_retries = 100
+    cur_retries = 0
+    while chromium_pid = None and cur_retries < max_retries:
+        time.sleep(5)
+        
+        for proc in psutil.process_iter(attrs=['pid', 'name', 'cmdline']):
+            try:
+                cmdline = ' '.join(proc.info['cmdline'])
+                if ('chromium' in proc.info['name'].lower() or 'chrome' in proc.info['name'].lower()) \
+                and target_args in cmdline:
+                    logger.log_event("Found PID of new Chromium process, {}, after {} retries".format(proc.info['pid'], cur_retries))
+                    chromium_pid = proc.info['pid']
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+        
+        cur_retries = cur_retries + 1
+
+    return chromium_pid
